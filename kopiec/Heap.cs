9025c7deee53624d236367a6717002d98b37f066
KwaśnyJestem <Kamil.Kwasny@microsoft.wsei.edu.pl>
2022-04-23 15:15:24 +0200

    zrobienie1

---
+++ kopiec/Heap.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Heap<T> where T : IComparable<T>
+{
+    private List<T> list;
+    public HeapOptions Option { get; }
+
+    // tworzy pusty kopiec dla określonego porządku
+    public Heap(HeapOptions option = HeapOptions.MinHeap)
+    {
+        this.Option = option;
+        list = new List<T>();
+    }
+    // ... uzupełnij kod
+
+    public Heap(IEnumerable<T> collection, HeapOptions option = HeapOptions.MinHeap) : this(option)
+    {
+        foreach(var element in collection)
+        {
+            Insert(element);
+        }
+    }
+
+    public int Count => list.Count;
+
+
+    public void Insert(T x)
+    {
+        throw new NotImplementedException(); //ToDo
+    }
+
+
+    public T Delete()
+    {
+        throw new NotImplementedException(); //ToDo
+    }
+
+
+    public T Top()
+    {
+        return list[0]; //korekta tutaj bedzie potrzebna
+    }
+
+
+    public void Clear() => list.Clear();
+
+
+    public T[] ToArray() => list.ToArray();
+
+}
--- kopiec/Program.cs
+++ kopiec/Program.cs
@@ -2,11 +2,28 @@
 
 namespace kopiec
 {
+    public enum HeapOptions { MaxHeap = -1, MinHeap = 1 }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // MaxHeap, tworzenie kopca
+            var heap = new Heap<int>(new int[] { 2, 1, 6, 7, 1, 3 }, HeapOptions.MaxHeap);
+            int[] tab = heap.ToArray();
+            Console.WriteLine(string.Join(' ', tab));
+            Console.WriteLine("Element szczytowy: " + heap.Top());
+            Console.WriteLine("Liczba elementów: " + heap.Count);
+            heap.Clear();
+            Console.WriteLine("Liczba elementów po czyszczeniu: " + heap.Count);
+            try
+            {
+                Console.WriteLine(heap.Top());
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Wyjątek przechwycony");
+            }
         }
     }
 }
